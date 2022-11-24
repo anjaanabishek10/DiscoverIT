afff1cdba00441cd090af1ba32bc64de7cb0fb08
Balagurusee1 <115578255+Balagurusee1@users.noreply.github.com>
2022-11-24 20:56:46 +0530

    Add files via upload

---
+++ Balaguru/day3.py
@@ -0,0 +1,13 @@
+a="balaguru"
+b="25"
+print(a+b)
+
+a="balaguru 312819104015"
+print(a)
+
+a="I love this world"
+print(a[10],a[2:],a[:6],a[7:17])
+
+
+a="aravind like{}abishek like{}balaguru like{}snacks"
+print(a.format{"biscuits","burfee","chocolate"})
