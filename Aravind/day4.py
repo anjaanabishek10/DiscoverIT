877c5dc65f146e6ffc54946042cd180a9e440a30
aravind240 <115577585+aravind240@users.noreply.github.com>
2022-11-25 19:28:55 +0530

    Add files via upload

---
+++ Aravind/day4.py
@@ -0,0 +1,58 @@
+a="hEllo world"
+b=a.title()
+print(b)
+name="i love icream"
+new=name.capitalize()
+print(new)
+name="aravind is good"
+new=name.upper()
+print(new)
+a="REST IS end Not in THE MIDDLE"
+b=a.lower()
+print(b)
+str="breaKFAST Is A VeRy muST"
+b=str.swapcase()
+print(b)
+a="h i o open green"
+b=a.find("e")
+print(b)
+b="AraVind"
+a=b.replace('V','v')
+print(a)
+name="goodiboy"
+new=name.split('i')
+print(new)
+a="aravind is a good boy.He love python"
+b=len(a)
+print(b,a[::-1])
+a=[9,8,7,6]
+b=[5,4,3,2,1]
+c=a+b
+print(c)
+a=[1,1,1,3,4,2]
+print(a[:4])
+name=[1,2,3,6,5]
+name[3]=4
+print(name)
+a=[1,2,3,4,5,6,7,8]
+a.pop(4)
+print(a)
+name=[7,3,0,5,3,8,3,9,7,0]
+name.insert(4,2)
+print(name)
+a=[1,2,3,4]
+a.append(5)
+print(a)
+name=['a','f','g','r','y','w','s','d','c','v','b','n','m']
+name.sort()
+print(name,name[::-1])
+a=['q','a','c','v','f','b','z','x','h','u','n','j','l','s','a','a','w','q','z','c','z']
+b=a.count('a')
+print(b)
+name=['a','r','a','v','i','n','d']
+new=name.index('i')
+print(new)
+a=[1,4,7,9,0,8,5]
+b=len(a)
+print(b)
+
