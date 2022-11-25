bc80153e0568f3054532649358b20d1f444f73b5
Balagurusee1 <115578255+Balagurusee1@users.noreply.github.com>
2022-11-25 20:03:26 +0530

    Add files via upload

---
+++ Balaguru/day4.py
@@ -0,0 +1,46 @@
+a="i love unnatha"
+out=a.title()
+print(out)
+
+b="25good"
+out=b.title()
+print(out)
+
+a="python developer"
+out=a.swapcase()
+print(out)
+
+a="ethula g yenga erukunu kandu pudi papom"
+out=a.find("g")
+print(out)
+
+a="mm ok ethula yenga erukunu kandu pudi papom"
+out=a.find("s")
+print(out)
+
+a="hero"
+b=a.replace("h","z")
+print(b)
+
+a=[10,20,5,30,7]
+a[0]=5
+a[1]=1
+a[3]=1
+print(a)
+
+a=[1,2,2,3,4,5]
+a.remove(2)
+a.remove(2)
+print(a)
+
+a=[1,5,10,15,20]
+a.insert(3,25)
+print(a)
+
+b=[1,8,5,13,9,11]
+b.sort()
+print(b)
+
+a=[1,2,3,4,2]
+print(a.index(3))
+
