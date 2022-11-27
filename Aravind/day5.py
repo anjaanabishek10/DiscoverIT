f953467e124124d793d168c1bf8f1b9987e3aab4
aravind240 <115577585+aravind240@users.noreply.github.com>
2022-11-27 11:04:58 +0530

    Add files via upload

---
+++ Aravind/day5.py
@@ -0,0 +1,16 @@
+a=['a','r','a','v','i','n','d',1,2,3,4,4]
+b=len(a)
+a[5]=7
+a.pop(4)
+a.remove(1)
+c=[0,8,2,1,3,5,7,4,6]
+c.sort()
+c.insert(9,10)
+print(b,a,c)
+a=[1,2,3,4]
+b=[5,6,7,8]
+c=a+b
+print(c)
+fname='Aravind'
+lname='\tPriya'
+print(fname+lname)
