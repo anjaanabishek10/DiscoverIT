840abbe64d92b0e25356ad3199f6b18ff5efacbd
aravind240 <115577585+aravind240@users.noreply.github.com>
2022-12-06 20:00:52 +0530

    Add files via upload

---
+++ Aravind/day15.py
@@ -0,0 +1,19 @@
+a=7
+b=8
+c=9
+print(a&b)
+print(c|a)
+print(a^b)
+print(~c)
+print(c<<a)
+print(b>>c)
+
+name='aravind','priya'
+new='balaguru','abis'
+if (name==new):
+    print('same')
+elif (name>=new):
+    print('greater') 
+else:
+    print('different')        
+       
