e190e1ccd8b8ca040cce45e832cdeab381a46526
aravind240 <115577585+aravind240@users.noreply.github.com>
2022-12-06 20:00:27 +0530

    Add files via upload

---
+++ Aravind/day14.py
@@ -0,0 +1,14 @@
+a=33
+b=200
+if (b<a):
+    print("b is a less")
+elif (a>b):
+    print("a is a greater")
+elif (a==b):
+    print("same")
+else:
+    print("average")            
+a=[1,2,3,4,5]
+b=[6,7,8,9]
+print(7 is a )
+print(6 is not a )    
