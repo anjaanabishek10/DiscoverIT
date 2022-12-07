b18d65eeac823c2f57aa5d1a48661630dc9a0dd7
aravind240 <115577585+aravind240@users.noreply.github.com>
2022-12-07 20:58:43 +0530

    Add files via upload

---
+++ Aravind/day16.py
@@ -0,0 +1,13 @@
+a=98
+b=90
+if (a==b):
+    print('same')
+elif(a<=b):
+    print('greater')
+elif(b>=a):
+    print('less')
+elif(a!=b):
+    print('not equal')
+else:
+    print('minus')
+                
