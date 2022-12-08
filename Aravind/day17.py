a76011d2c93bd311299d8517539c8814180aadbb
aravind240 <115577585+aravind240@users.noreply.github.com>
2022-12-08 21:43:48 +0530

    Add files via upload

---
+++ Aravind/day17.py
@@ -0,0 +1,20 @@
+name=['a','r','a','v','i','n','d']
+for i in name:
+    print(i)
+i=1
+while i<6:
+    print(i)
+    if(i==4):
+        break
+    i+=1
+i=0
+while i<6:
+    i+=1
+    if (i==4):
+        continue
+    print(i)
+i=10  
+for i in range(10):
+    print(i)
+else:
+    print(i)      
