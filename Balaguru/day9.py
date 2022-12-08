e6dd1d15674f5b22be0824646804f6d519977b77
Balagurusee1 <115578255+Balagurusee1@users.noreply.github.com>
2022-12-08 20:58:00 +0530

    Add files via upload

---
+++ Balaguru/day9.py
@@ -0,0 +1,22 @@
+a=20
+b=10
+if(a==b):
+    print("a is equal to b")
+else:
+    print("a is not equal to b")
+    print(a!=b)
+    print(a>b)
+    print(a<b)
+    print(a>=b)
+    print(a<=b)
+    print(b>=a)
+
+    a=5
+    b=6
+    print((a>b)or(b<=a))
+    a=5
+    b=6
+    print((a==b)or(b<a))
+    a=5
+    b=10
+    print(not a==b)
