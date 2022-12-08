df6f93d9b4fd72b6c33e17e18480c8a3650c93d9
Balagurusee1 <115578255+Balagurusee1@users.noreply.github.com>
2022-12-08 20:46:50 +0530

    Add files via upload

---
+++ Balaguru/day6.py
@@ -0,0 +1,25 @@
+a="123"
+b=str(a)
+c=(b[0])+(b[1])+(b[2])
+print(c)
+
+a="bala"
+b=str(a)
+c=(b[0])+(b[1])+(b[2])+(b[3])
+print(c)
+
+a="123"
+b=int(a)
+print(a)
+
+num = '10'
+print(type(num))
+converted_num=int(num)
+print(type(converted_num))
+print(converted_num+20)
+
+a=5
+b=5.5
+sum=a+b
+print(sum)
+print(type(sum))
