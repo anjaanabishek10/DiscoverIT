6780d6b708ae8a959d2017a6ed5cd9cef5fafd3f
aravind240 <115577585+aravind240@users.noreply.github.com>
2022-12-09 18:38:18 +0530

    Add files via upload

---
+++ Aravind/day18.py
@@ -0,0 +1,14 @@
+for i in range(10):
+    pass
+else:
+    print(i)
+    
+count=1
+while (count<5):
+    count=count+1
+    print('count')
+
+a=[2,3,4,5,6,7]    
+for i in a:
+    print(i)
+           
