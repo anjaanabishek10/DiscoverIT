0dc833151b8190ffebb5dc2e036c58f4e842351a
aravind240 <115577585+aravind240@users.noreply.github.com>
2022-12-11 19:14:44 +0530

    Add files via upload

---
+++ Aravind/day20.py
@@ -0,0 +1,23 @@
+def aravind():
+    fname='aravind'
+    lname='priya'
+    b=fname.capitalize()
+    a=lname.capitalize()
+    print(b+a)
+aravind()
+aravind()
+aravind()
+def aravi1(a,b):
+    n=a.upper()
+    m=b.upper()
+    return(n+m)
+print(aravi1('aravind',' priya'))
+
+def sum(a,b):
+    print(a<100 and a>100)
+    print(a<100 and a>=75)
+    print(not(b>90 and b==80))
+    return(a)
+sum(80,100)
+        
+               
