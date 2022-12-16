9098e53e11aca32592c8ce962cf048381de316c6
Balagurusee1 <115578255+Balagurusee1@users.noreply.github.com>
2022-12-16 20:33:57 +0530

    Add files via upload

---
+++ Balaguru/day12.py
@@ -0,0 +1,22 @@
+a = 10  
+b = 20  
+if a<b:  
+  print("a is less than b")
+
+  
+a = 10  
+b = 20  
+if a==b:  
+  print("a and b are equal")  
+else:  
+  print("a and b are not equal")  
+
+  a=int(input("enter the a value"))
+if a> 10:  
+  print("Above 10")  
+  if a > 100:  
+    print("and also above 100")  
+  else:  
+    print("and also below 100")  
+else:  
+    print("below 10")  
