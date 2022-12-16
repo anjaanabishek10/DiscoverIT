b6946644ad97cd38a41ee54e43bca0d44d260f22
aravind240 <115577585+aravind240@users.noreply.github.com>
2022-12-16 19:20:39 +0530

    Add files via upload

---
+++ Aravind/day22.py
@@ -0,0 +1,9 @@
+x=min(70,87,90)
+y=max(56,87,43)
+print(x)
+print(y)
+num = int(input("Enter a number: "))
+if (num % 2) == 0:
+   print("{0} is Even".format(num))
+else:
+   print("{0} is Odd".format(num))
---
+++ Aravind/day25.py
@@ -0,0 +1,11 @@
+import calendar
+
+yy = 2023 
+mm = 1   
+
+print(calendar.month(yy, mm))
+import array as i 
+a = i.array('i', [2, 4, 6, 8])  
+print("First element:", a[0])  
+print("Second element:", a[1])  
+print("Second last element:", a[-1]) 
