5fa8744dd677dfecf026321f6fea811a5b3322b8
Balagurusee1 <115578255+Balagurusee1@users.noreply.github.com>
2022-12-16 20:35:24 +0530

    Add files via upload

---
+++ Balaguru/day14.py
@@ -0,0 +1,24 @@
+def sum():
+  a=10
+  b=20
+  print(a+b)
+sum()
+def sum():
+    a="bala"
+    b="guru"
+    print(a+b)
+sum()
+
+def factorial_number():
+    factorial=1
+    fact_number=int(input("enter a number to get its factorial:"))
+    for i in range (1,fact_number+1):
+        factorial=factorial*i
+    print("the factorial is factorial",factorial)
+factorial_number()
+
+def sum3():
+     a=5
+     b=10
+     return(a+b)
+print(sum3())
