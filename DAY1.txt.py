8dfa19450950505ea5f081b5664a95e53283522b
Suppiriya <129966520+Suppiriya@users.noreply.github.com>
2023-04-09 22:21:00 +0530

    Add files via upload

---
+++ DAY1.txt.py
@@ -0,0 +1,24 @@
+Name = "priya"
+age = int(19.231)
+color = "pink"
+print(Name)
+print(age)
+print(color)
+a = b = c = 3
+print(c)
+a = 2
+a = 3
+c = 4
+print(a,b,c)
+d = "number"
+print(d)
+aa = 123
+bb = 12.38
+cc = 16+1j
+print(aa,bb,cc)
+students = ["priya,aravind"]
+print(students)
+e = range(15)
+print(e)
+f = {1:"a",2:"aa"}
+print(f)
