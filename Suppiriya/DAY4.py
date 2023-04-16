52c6bceb0f63d23667293c8ebf3deddbc6d10473
Suppiriya <129966520+Suppiriya@users.noreply.github.com>
2023-04-16 22:48:17 +0530

    Add files via upload

---
+++ Suppiriya/DAY4.py
@@ -0,0 +1,64 @@
+hi = "hello"
+a = ["priya", 2, hi]
+print(a)
+top = "list concatenation"
+print(top)
+agni = ["CSE","IT"]
+srm = ["ECE","EEE"]
+college = agni+srm
+print(college)                  
+top2 = "slicing"
+print(top2)
+aa = [0,1,2,3,4,5,6,7,8,9,10]
+print(aa[3])
+print(aa[:2])
+print(aa[:2:7])
+print(aa[::-1])
+top3 = "update"
+print(top3)
+bb = [0,1,2,3,4,5,6,7,8,9,10]
+bb[8] = 18
+print(bb)
+top4 = "delete"
+print(top4)
+cc = [0,1,2,3,4,5,6,7,18,9,10]
+cc.remove(18)
+print(cc)
+dd = [0,1,2,3,4,5,6,7,18,9,10]
+dd.pop(8)
+print(dd)
+top5 = "insert"
+print(top5)
+b = [0,1,2,3,4,5,6,7,18,9,10]
+b.insert(8,"A")
+print(b)
+top6 = "append"
+print(top6)
+c = [0,1,2,3,4,5,6,7,18,9,10]
+c.append(12345)
+print(c)
+top7 = "sort"
+print(top7)
+d = [2,5,78679,0,4,66,246,458,38,496,446]
+d.sort()
+print(d[::-1])
+print(d)
+top8 = "reverse"
+print(top8)
+e = [2,5,78679,0,4,66,246,458,38,496,446]
+e.reverse()
+print(e)
+top9 = "count"
+print(top9)
+f = [2,2,3,4,5,5,6,7,7,7,8,6,65,3,2,2,2,]
+print(f.count(7))
+top10 = "index"
+print(top10)
+g = [2,5,78679,0,4,66,246,458,38,496,446]
+print(g.index(78679))
+top11 = "len"
+print(top11)
+h = [2,5,78679,0,4,66,246,458,38,496,446]
+print(len(h))
+j = ["s","u"]
+print(len(j))
